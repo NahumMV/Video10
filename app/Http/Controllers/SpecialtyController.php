0d04e168dde853cd714b7f74f222a74c189e8ca4
NahumMV <thenahum1@gmail.com>
2023-08-20 02:08:40 -0400

    avance

---
+++ app/Http/Controllers/SpecialtyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Specialty;
+
+class SpecialtyController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    
+    
+    public function index(){
+        $specialties = Specialty::all();
+        return view('specialties.index',compact('specialties'));
+    }
+
+    public function create(){
+        return view('specialties.create');
+    }
+
+    public function sendData(Request $request){
+        $specialty = new Specialty();
+        $specialty ->name = $request->input('name');
+        $specialty ->description = $request->input('description');
+        $specialty ->save();
+
+        return redirect('/especialidades');
+    }
+}
